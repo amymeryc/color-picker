1d40d438d70713aa6202dad5d50b7343ae4e7619
Amy <amymerydesigns@gmail.com>
2026-03-02 22:52:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // ESTA ES LA LÍNEA MÁGICA PARA GITHUB PAGES:
+ 
     base: '/color-picker/', 
     
     plugins: [react(), tailwindcss()],
